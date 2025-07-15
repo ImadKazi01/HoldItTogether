64892f49464238b54d8a96a5bf79c50eeb2e338a
Imad <hello@imadkazi.co.uk>
2025-07-15 13:17:08 +0100

    Initial Commit

---
+++ HoldItTogetherTests/HoldItTogetherTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoldItTogetherTests.swift
+//  HoldItTogetherTests
+//
+//  Created by Imad Kazi on 15/07/2025.
+//
+
+import Testing
+@testable import HoldItTogether
+
+struct HoldItTogetherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ HoldItTogetherUITests/HoldItTogetherUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HoldItTogetherUITestsLaunchTests.swift
+//  HoldItTogetherUITests
+//
+//  Created by Imad Kazi on 15/07/2025.
+//
+
+import XCTest
+
+final class HoldItTogetherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
